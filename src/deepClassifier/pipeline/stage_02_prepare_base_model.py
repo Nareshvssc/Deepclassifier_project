9747fa75dddf82cf490ceddfb6914b7b860da6da
Nareshvssc <nareshbtkit@gmail.com>
2022-12-13 20:19:30 +0530

    PrepareBasemodel

---
+++ src/deepClassifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,21 @@
+from deepClassifier.config import ConfigurationManager
+from deepClassifier.components import PrepareBaseModel
+from deepClassifier import logger
+
+STAGE_NAME="Preparation of base model stage"
+
+def main():
+    config = ConfigurationManager()
+    prepare_base_model_config = config.get_base_model_config()
+    prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+    prepare_base_model.get_base_model()
+    prepare_base_model.update_base_model()
+
+
+if __name__=="__main__":
+    try:
+        logger.info(f"<<< stage {STAGE_NAME} started")
+        main()
+        logger.info(f"<<< stage {STAGE_NAME} ended")
+    except Exception as e:
+        raise e
